8b8517473c4d34f69493dccc66807776d6d3ecbd
reuhdz <30872054+reuhdz@users.noreply.github.com>
2019-03-03 16:37:18 -0500

    Update loginUser.php
    - added function to prevent sql injection
    -updated query statement to work 2018spf db
    - updated cookie values

--- public_html/controller/loginUser.php
+++ public_html/controller/loginUser.php
@@ -1,11 +1,13 @@
 <?php
-	include ("connection.php");
-	$username = $_POST['username'];
-	$password = $_POST['pass'];
+	include 'dbconfig.php';
+	$conn = Connect();
+	$username = mysqli_real_escape_string($conn, $_POST['username']);
+	$password = mysqli_real_escape_string($conn, $_POST['pass']);
+	$encrypt = hash("sha256", $password);
 
 	if(!empty($username) && !empty($password)) {
 			    //Fetch from database
-			    $query = "select * from 2018S_shelara.spf_user where email = '$username' and password = '$password';";
+			    $query = "SELECT username, passwd, fname, lname from 2018spfdb.Users where username = '$username' and passwd = '$encrypt'";
 			   
 			    $result = mysqli_query($conn,$query);
 				if(!$result) {
@@ -15,17 +17,17 @@
 			     
 				if ($count == 1){
 					$row = mysqli_fetch_assoc($result);
-					$uid = $row['uid'];
+					$uid = $row['u_id'];
 
 					header("Location:../view/dashboard.html");
 					
 					//store user details in cookie
 					setcookie("userId", $uid, time() + (86400 * 30), "/");
-					setcookie("userFName", $row['first_name'], time() + (86400 * 30), "/");
-					setcookie("userLName", $row['last_name'], time() + (86400 * 30), "/");
+					setcookie("userFName", $row['fname'], time() + (86400 * 30), "/");
+					setcookie("userLName", $row['lname'], time() + (86400 * 30), "/");
 				}else{
-					$message = "Please enter a valid email id and password.";
-					header("Location:../view/login.html?result=Please enter a valid email id and password.");
+					$message = "Please enter a valid username and password.";
+					header("Location:../view/login.html?result=Please enter a valid username and password.");
 					//echo $message;
 				}
 			}

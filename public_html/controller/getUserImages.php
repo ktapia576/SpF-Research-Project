50565c6aa1da7f8e90ab2f17caee82ba1908ae40
ktapia576 <ktapia576@hotmail.com>
2019-04-06 00:08:42 -0400

    Changed from GET to COOKIE for userId Fetch and Added else statement for debugging purposes

--- public_html/controller/getUserImages.php
+++ public_html/controller/getUserImages.php
@@ -1,9 +1,9 @@
 <?php
 	include 'dbconfig.php';
 	$conn = Connect();
-	$userId = $_GET['userId']; // Get userId created from cookie and passed as GET method through AJAX
+	$userId = $_COOKIE['userId']; // Get userId created from cookie and passed as GET method through AJAX
 
-	$imageQuery = "SELECT * FROM 2018spfdb.Images WHERE userId = '$userId'";
+	$imageQuery = "SELECT * FROM 2018spfdb.Images WHERE userID = '$userId'";
 	$imageQueryResult = mysqli_query($conn, $imageQuery);
 	if(!$imageQueryResult) {
 		die("Database query failed: " . mysqli_error($conn));
@@ -13,8 +13,11 @@
 
 	if ($countImage > 0){
 		while($row = mysqli_fetch_array($imageQueryResult, MYSQLI_ASSOC)){
-	        $returnVar[] = $row;
+			$returnVar[] = $row;
 		}
+	}
+	else {
+		echo "No Images Found. CountImage=".$countImage.", userid=".$userId;
 	}
 
     echo json_encode($returnVar);

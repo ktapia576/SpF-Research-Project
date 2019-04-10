e53a92f50d34d1b46e1a977bf72bf66183fff37a
reuhdz <30872054+reuhdz@users.noreply.github.com>
2019-04-10 13:11:08 -0400

    more test scripts, created a test folder for tensorflow

--- public_html/tensorFlow/people_test.sh
+++ public_html/tensorFlow/people_test.sh
@@ -1,3 +1,3 @@
 #sh people_test.sh
 
-python3 label_image1.py --graph=people_output_graph.pb --labels=people_output_labels.txt --input_layer=Placeholder --output_layer=final_result --image=../UserImg/test/*.png
+python3 label_image2.py --graph=people_output_graph.pb --labels=people_output_labels.txt --input_layer=Placeholder --output_layer=final_result --image= UserImg/test/1553532713638.png
